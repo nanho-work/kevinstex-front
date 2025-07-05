45c8b8d76cc9d3d668d9174a3a44c8987b875467
nhchoi2 <choi1278@gmail.com>
2025-07-05 13:21:23 +0900

    1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,6 @@ export default function Home() {
     <main className="min-h-screen bg-white text-gray-900 flex flex-col items-center justify-center p-8">
       <Hero/>
       <ReviewSliderSection />
-
     </main>
   )
 }
