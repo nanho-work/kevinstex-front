54dca1bfa0faea7652e2f6cf07da5f0516145748
nhchoi2 <choi1278@gmail.com>
2025-07-05 14:10:10 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
+  basePath: isProd ? '/kevinstex-front' : '',
   assetPrefix: isProd ? '/keinsrex-front/' : '', // GitHub Pages + 도메인 대응
   images: {
     unoptimized: true, // next/image 최적화 비활성화 (static export용)
