a092e63541dbb1ee68193b91b64a86f83c9ea9f4
nhchoi2 <choi1278@gmail.com>
2025-07-05 23:43:44 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,8 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   output: 'export', // ✅ static export 사용 (next export 전용)
   trailingSlash: true,
-  assetPrefix: isProd ? '/' : '', // ✅ "/" 루트 도메인용 프리픽스 (GitHub Pages 루트 도메인에 연결 시)
+  assetPrefix: isProd ? '/' : '', 
+  basePath: '',// ✅ "/" 루트 도메인용 프리픽스 (GitHub Pages 루트 도메인에 연결 시)
   images: {
     unoptimized: true, // ✅ next/image 비활성화 → 정적 export에 맞춤
   },
