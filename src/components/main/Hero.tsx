19cdf9597f8bdc06b43b02d31a1151781a811b96
nhchoi2 <choi1278@gmail.com>
2025-08-14 10:05:56 +0900

    1

--- src/components/main/Hero.tsx
+++ src/components/main/Hero.tsx
@@ -5,7 +5,15 @@ const prefix = ''
 
 export default function Hero() {
   return (
-    <section className="w-screen bg-gradient-to-l from-blue-200 to-white hero-font">
+    <section
+      className="w-screen hero-font bg-cover bg-center min-h-[700px]"
+      style={{
+        backgroundImage: `
+      linear-gradient(to left, rgba(191, 219, 254, 0.3), rgba(255, 255, 255, 0.5)),
+      url('/main_bg1.jpg')
+    `
+      }}
+    >
       <div className="max-w-7xl mx-auto w-full grid md:grid-cols-2 items-start gap-10">
         {/* 좌측 문구 */}
         <div className="flex flex-col min-h-[400px] md:h-[500px] justify-center space-y-6 px-6">
@@ -20,7 +28,7 @@ export default function Hero() {
             복잡한 세무, 전문가가 간단하게 해결해드립니다.
           </p>
           <a
-            href="https://map.naver.com/p/entry/place/1166913410?lng=127.1221551&lat=37.4883000&placePath=/home?bookingRedirectUrl=https%3A%2F%2Fm.booking.naver.com%2Fbooking%2F5%2Fbizes%2F1171914&theme=place&entry=pll&lang=ko&from=map&fromPanelNum=1&additionalHeight=76&timestamp=202507101558&locale=ko&svcName=map_pcv5&theme=place&entry=pll&lang=ko&from=map&fromPanelNum=1&additionalHeight=76&timestamp=202507101558&locale=ko&svcName=map_pcv5&area=pll&c=15.00,0,0,0,dh"
+            href="https://map.naver.com/p/entry/place/1166913410..."
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white px-4 py-2 rounded-lg shadow bg-gradient-to-r from-blue-800 to-blue-400 hover:from-blue-900 hover:to-blue-500 transition text-center w-fit"
@@ -32,9 +40,9 @@ export default function Hero() {
         {/* 우측 이미지 */}
         <div className="hidden md:flex relative md:h-[500px] w-full items-center justify-center">
           <img
-            src={`${prefix}/kwon_profile.png`}
+            src={`/kwon-any.png`}
             alt="권도윤 송파세무사 | 디케빈즈택스랩, 케빈즈택스의 권도윤세무사가 세금과 세무를 책임집니다."
-            className="h-[180%] object-contain absolute mt-[85px]"
+            className="h-[150%] object-contain absolute mt-[150px]"
           />
         </div>
       </div>
